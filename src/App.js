e6c3610247a465e0157432fc5c60702bdf43c6cf
git <nguyenkimcong1999@gmail.com>
2022-12-28 22:49:10 +0700

    bkt

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Cart from './components/shoppingcart/Cart';
+
+function App() {
+  return (
+    <>
+    <Cart/>
+    </>
+  );
+}
+
+export default App;
